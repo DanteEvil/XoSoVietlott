b292a66526059e4e7962fb9e323a1b653b8bf71d
Le Ngo Quang Truong <quang-truong.le-ngo@ekino.com>
2019-08-25 02:25:08 +0700

    Update script

--- XoSoVietlott/appcenter-post-clone.sh
+++ XoSoVietlott/appcenter-post-clone.sh
@@ -7,3 +7,4 @@ COCOAPODS_VER=`sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' Podfile.lock`
 
 echo "Installing CocoaPods version $COCOAPODS_VER"
 sudo gem install cocoapods -v $COCOAPODS_VER
+pod install
